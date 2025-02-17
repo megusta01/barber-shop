7127ecdac05462ef05f1d770ce2547970cff2e7c
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2025-02-17 14:38:32 -0300

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
 import '../styles/globals.css'
 import React from 'react'
-import Header from '../components/Header'
 
 export const metadata = {
-  title: 'Minha Aplicação',
-  description: 'Gerenciamento de agendamentos',
+  title: 'Barbearia',
+  description: 'Sistema de agendamentos',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='pt-BR'>
       <body className='bg-gray-50 text-gray-800'>
-        <Header />
-        <main className='container mx-auto px-4 py-6'>{children}</main>
+        {children}
       </body>
     </html>
   )
