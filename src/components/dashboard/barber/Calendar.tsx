39d505b0be81192850fa6a34b84c5e8dc0ec63fb
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2025-02-27 14:15:23 -0300

    Calendário para agendamento

--- src/components/dashboard/barber/Calendar.tsx
+++ src/components/dashboard/barber/Calendar.tsx
@@ -1,10 +1,32 @@
-import {Calendar} from "@heroui/calendar";
+// import {Calendar} from "@heroui/calendar";
+// import React from "react";
+// import {parseDate} from "@internationalized/date";
+// import styles from "../../../styles/calendar.module.css";
+
+// export default function App() {
+//   let [value, setValue] = React.useState(parseDate("2024-03-07"));
+
+//   return <Calendar aria-label="Date (Controlled)" value={value} onChange={setValue} className={styles.calendar} />;
+// }
+
+import { Calendar } from "@heroui/calendar";
 import React from "react";
-import {parseDate} from "@internationalized/date";
+import { parseDate, today, getLocalTimeZone } from "@internationalized/date";
+import styles from "../../../styles/calendar.module.css";
 
 export default function App() {
   let [value, setValue] = React.useState(parseDate("2024-03-07"));
 
-  return <Calendar aria-label="Date (Controlled)" value={value} onChange={setValue} />;
+  return (
+    <div className={styles.calendarContainer}>
+      <h2 className="text-lg font-semibold text-gray-700 mb-2">Agendamento</h2>
+      <Calendar
+        aria-label="Calendário de Agendamentos"
+        value={value}
+        onChange={setValue}
+        className={styles.calendar}
+        minValue={today(getLocalTimeZone())}
+      />
+    </div>
+  );
 }
-
--- src/components/dashboard/client/Calendar.tsx
+++ src/components/dashboard/client/Calendar.tsx
@@ -1,12 +1,32 @@
-import {Calendar} from "@heroui/calendar";
-import {today, getLocalTimeZone} from "@internationalized/date";
+// import { Calendar } from "@heroui/calendar";
+// import { today, getLocalTimeZone } from "@internationalized/date";
+// import styles from "../../../styles/calendar.module.css";
+// export default function App() {
+//   return (
+//     <div className={styles.calendarContainer}>
+//       <Calendar
+//         className={styles.calendar}
+//         aria-label="Date (Min Date Value)"
+//         defaultValue={today(getLocalTimeZone())}
+//         minValue={today(getLocalTimeZone())}
+//       />
+//     </div>
+//   );
+// }
+
+import { Calendar } from "@heroui/calendar";
+import { today, getLocalTimeZone } from "@internationalized/date";
+import styles from "../../../styles/calendar.module.css";
 
 export default function App() {
   return (
-    <Calendar
-      aria-label="Date (Min Date Value)"
-      defaultValue={today(getLocalTimeZone())}
-      minValue={today(getLocalTimeZone())}
-    />
+    <>
+      <Calendar
+        className={styles.calendar}
+        aria-label="Calendário"
+        defaultValue={today(getLocalTimeZone())}
+        minValue={today(getLocalTimeZone())}
+      />
+    </>
   );
 }
