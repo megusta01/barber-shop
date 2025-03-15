9b92b458ca20320ce99b6bec3cf9233c4f8637ce
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2025-03-15 01:31:46 -0300

    Buscando id do cliente

---
+++ src/hooks/clientId.ts
@@ -0,0 +1,25 @@
+'use client'
+import {jwtDecode} from "jwt-decode";
+
+interface DecodedToken {
+  id?: string;  // Outros armazenam diretamente em 'id'
+}
+
+export default function getUserIdFromToken(): string | null {
+  const token = localStorage.getItem("accessToken");
+
+  if (!token) {
+    console.error("Token JWT não encontrado no localStorage");
+    return null;
+  }
+
+  try {
+    const decoded: DecodedToken = jwtDecode(token);
+    return decoded.id ||  null;
+  } catch (error) {
+    console.error("Erro ao decodificar o token:", error);
+    return null;
+  }
+};
+
+
