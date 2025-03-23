640f0729c54b8d7d411f4830d727fb93ed9b0890
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2025-03-23 15:45:49 -0300

    admin page

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
-import Header from "@/components/dashboard/client/Header"
-import AdminPanel from "@/components/dashboard/admin/AdminPanel"
+import AdminHeader from '@/components/dashboard/admin/Header'
+import AdminPanel from '@/components/dashboard/admin/AdminPanel'
+
 export default function AdminDashboard() {
-    return(
-        <>
-            <Header />
-            <AdminPanel />
-        </>
-    )
+  return (
+    <>
+      <AdminHeader />
+      <div className="min-h-screen bg-[#F5F6F8] px-4 py-10">
+        <div className="max-w-5xl mx-auto">
+          <AdminPanel />
+        </div>
+      </div>
+    </>
+  )
 }
